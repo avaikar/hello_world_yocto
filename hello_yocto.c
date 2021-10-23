4060ab3f6597f75db5981e163329796a0e5c4468
Anand <a.vaikar2021@gmail.com>
2021-10-23 13:28:50 +0530

    Yes I can commit from Ubuntu VM in Hyper-V

--- hello_yocto.c
+++ hello_yocto.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
    printf("Hello Yocto, I am Anand coding\n");
    
    printf("Checking if I can push from the VM environment\n");
+
+   printf("Wow, I definitely can and this is fun!!\n");
    
    return 0;
 }
