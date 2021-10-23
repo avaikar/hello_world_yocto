91cc3646a5a19b4fef42ce575226f70466a00159
Anand <a.vaikar2021@gmail.com>
2021-10-23 20:31:51 +0530

    Testing again

--- hello_yocto.c
+++ hello_yocto.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[]) {
    printf("Checking if I can push from the VM environment\n");
 
    printf("Wow, I definitely can and this is fun!!\n");
-   
+  
+   printf("Testing a few other things\n");
+
    return 0;
 }
